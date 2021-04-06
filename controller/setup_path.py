a9ff720aeb5a14e012e9b115018f987d90aeb899
Peng <penwei@ucdavis.edu>
2021-04-06 08:44:15 -0700

    small update

--- controller/setup_path.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-# Path settings
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(curr_dir)
-import_path = os.path.join(parent_dir, '.')
-sys.path.insert(0, import_path)
